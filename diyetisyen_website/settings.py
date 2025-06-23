9a0336fa3c363e57cec8e61b3199b4edf65224af
mertoxx <mertcanndemirr@gmail.com>
2025-06-24 00:12:10 +0300

    Fix requ.txt

--- diyetisyen_website/settings.py
+++ diyetisyen_website/settings.py
@@ -105,7 +105,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Media files
 MEDIA_URL = '/media/'
--- diyetisyen_website/urls.py
+++ diyetisyen_website/urls.py
@@ -1,11 +1,20 @@
-from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+# diyetisyen_website/urls.py
 
-def home(request):
-    return HttpResponse("Django app çalışıyor!")
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('main.urls')),
+    path('randevular/', include('appointments.urls')),
+    path('tarifler/', include('recipes.urls')),
+    path('basari-hikayeleri/', include('success_stories.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+# Media files için URL pattern (development için)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
