7adf47b9a2f4ad727f4f9ec45a0a0f3f0cc4e8b0
mertoxx <mertcanndemirr@gmail.com>
2025-06-24 00:04:09 +0300

    Fix Vercel deployment configuration

--- diyetisyen_website/wsgi.py
+++ diyetisyen_website/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diyetisyen_website.settings')
 
 application = get_wsgi_application()
+
+# Vercel için
 app = application
